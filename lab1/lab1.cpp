eed63fbfd1880e792e086f51fec80b8179a1dc7a
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-08-25 05:04:40 +0000

    Lab 1 folder

---
+++ lab1/lab1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout<<" ~~~~~                        ~~~~~";
+    cout<< endl;
+    cout <<"  \ \                        / /";
+    cout<< endl;
+    cout <<"   \ \                      / /";
+    cout<< endl;
+    cout <<"    \ \     ~~~~~~~~~~~    / /";
+    cout<< endl;
+    cout <<"     \ \   /           \  / /";
+    cout<< endl;
+    cout <<"      \ \ /             \/ /";
+    cout<< endl;
+    cout <<"       \ {   O      O    }/";
+    cout<< endl;
+    cout <<"        \ \    T  T     //";
+    cout<< endl;
+    cout <<"         \ \   L__J    //";
+    cout<< endl;
+    cout <<"          \ \_________//";
+    cout<<endl;
+
+return 0;
+}
