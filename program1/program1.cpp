09426cd47acabdfe80896ed74b6a633cbe1f6365
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-09-14 18:11:04 +0000

    Program 1

---
+++ program1/program1.cpp
@@ -0,0 +1,59 @@
+/*
+creator: Malene Kavanagh 
+date: 9/12/16
+purpose: finding the wages for a person
+
+needs:
+at least 3 variables
+at least one equation
+input and output
+header comments
+embeded comments based on pseudocode
+*/
+
+#include <iostream> 
+using namespace std;
+
+int main(){
+    int hours;       // these are the variables
+    float hour_wage;
+    int week_days;
+    float week_wage;
+    float extra_var;
+    float taxes = 0.17;
+    float taxed_money;//taxed variable = net pay
+    
+    //the other variables
+    string name;
+    float daily_rate; //what you get in a day
+    
+    std::cout << "We are going to calculate wages" << std::endl;  // these will aquire the variables needed
+    std::cout << "input a name" << std::endl;
+    std::cin >> name;
+    
+    std::cout << "input number of working hours" << std::endl;
+    std::cin >> hours;
+    
+    std::cout << "input wage per hour" << std::endl;
+    std::cin >> hour_wage;
+    
+    std::cout << "input how many days of the week worked" << std::endl;
+    std::cin >> week_days;
+    
+    //calculating hours, rate, gross pay, and net pay
+    
+    daily_rate = (hours * hour_wage);
+    week_wage = (daily_rate) * week_days; //this is the equation used
+    extra_var = week_wage * taxes;
+    taxed_money = week_wage - extra_var;
+    
+    // wage slip : name, hours, daily rate, gross pay(for a week), and net pay(taxed gross pay)
+   
+   std::cout << "Wage Slip" << std::endl;
+   std::cout << "Name: " << name << std::endl;
+   std::cout << "Hours per day: " << hours << std::endl;
+   std::cout << "Daily rate: $" << daily_rate << std::endl;
+   std::cout << "Gross pay for the week: $" << week_wage << std::endl;
+   std::cout << "Taxed 17%" << std::endl;
+   std::cout << "Net pay for the week: $" << taxed_money << std::endl;
+}
