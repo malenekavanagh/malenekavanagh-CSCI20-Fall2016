187b3e767343f77040fe81976aa38262ddccd42e
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-10-05 05:14:28 +0000

    Lab10 submission

---
+++ lab10/lab10.cpp
@@ -0,0 +1,96 @@
+/*
+creator: Malene Kavanagh
+date: 10/3/2016
+purpose: running the equations from the work sheet
+
+requirements:
+use complete sentences in your output
+no input
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    std::cout << "problem 1" << std::endl;
+    
+    int suzyAge = 25;
+    int johnAge = 21;
+    
+    std::cout << "suzy's age is " << suzyAge << " and johns's age is " << johnAge << std::endl;
+    std::cout << suzyAge << " is less than " << johnAge << std::endl;
+    std::cout << "this is false." << std::endl;
+    
+    std::cout << "problem 2" << std::endl;
+    
+    int x = 7;
+    int y = 7;
+    
+    std::cout << "x = 7 and y = 7" << std::endl;
+    std::cout << x << " is greater than or equal to " << y << std::endl;
+    std::cout << "This is true" << std::endl;
+    
+    std::cout << "problem 3" << std::endl;
+    
+    int a = 1;
+    int b = 9;
+    
+    std::cout << "a = 1 and b = 9" << std::endl;
+    std::cout << a << " is equal to " << b << std::endl;
+    std::cout << "this is false" << std::endl;
+    
+    std::cout << "problem 4" << std::endl;
+    
+    int limit = 20;
+    int count = 10;
+    
+    std::cout << "limit = 20 and count = 10" << std::endl;
+    std::cout << "(" << limit << " * " << count << " ) / 2 is greater than 0" << std::endl;
+    std::cout << "this is true" << std::endl;
+    
+    std::cout << "problem 5" << std::endl;
+    
+    int t = -4;
+    int z = 0;
+    
+    std::cout << "t = -4 and z = 0 " << std::endl;
+    std::cout << t << " is greater than 5 or " << z << " is less than 2" << std::endl;
+    std::cout << "this statement is true" << std::endl;
+    
+    std::cout << "problem 6" << std::endl;
+    
+    int variable = -5;
+    
+    std::cout << "variable = -5" << std::endl;
+    std::cout << "false( " << variable << " greater than 0 )" << std::endl;
+    std::cout << "this is true" << std::endl;
+    
+    std::cout << "problem 7" << std::endl;
+    
+    int a2 = 16;
+    
+    std::cout << "a = 16 " << std::endl;
+    std::cout << a2 << "/ 4 less than 8 and  " << a2 << " greater than or equal to 4" << std::endl;
+    std::cout << "this is true" << std::endl;
+    
+    std::cout << "problem 8" << std::endl;
+    
+    int x2 = -2;
+    int y2 = 5;
+    
+    std::cout << x2 <<" * " << y2 << " is less than 10 or " << y << " * " << z << " is less than 10" << std::endl;
+    std::cout << "this is true" << std::endl;
+    
+    std::cout << "problem 9" << std::endl;
+    
+    int j = -2;
+    int k = 5;
+    int l = 4;
+    
+    std::cout << "j = -2, k = 5 and l = 4" << std::endl;
+    std::cout << "!( " << j << " * " << l << " is less than 10) or " << y2 << " / " << x2 << " * 4 is less than " << y2 << " * 2" << std::endl;
+    std::cout << "this is true" << std::endl;
+    
+}
