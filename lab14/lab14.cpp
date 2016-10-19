7803943b5f9b53fab498e18bd57cc50286948b1c
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-10-19 16:29:50 +0000

    Lab14 Submission

---
+++ lab14/lab14.cpp
@@ -0,0 +1,38 @@
+/*
+creator: Malene Kavanagh
+date:10/17/16
+puporse: find, 
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){  
+    
+    // initialization 
+    
+    int num;       
+    int sum = 0;   
+    
+    std::cout << "Enter numbers separated by spaces" << " and terminated with a negative number." << endl;
+    
+    // read first number  
+    
+    std::cin >> num;  
+    
+    while (num >= 0)  { 
+        
+            // add in to the sum
+            sum = sum + num;
+        
+            // read in next number (getting ready for next loop test)  
+        
+            std::cin >> num; /*sum changed to num*/
+    
+    }
+    
+    std::cout << "Sum is " << sum << endl; // was changed to is
+    
+    return 0;
+    
+}
