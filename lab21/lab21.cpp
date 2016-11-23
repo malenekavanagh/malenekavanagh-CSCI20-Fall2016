1bce361f134148943d1f3e7f790ac85ceacabf5b
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-11-23 18:38:49 +0000

    Lab21 submission

---
+++ lab21/lab21.cpp
@@ -0,0 +1,12 @@
+#include "include.h"
+#include "MyClass.cpp"
+
+// Program starts here
+int main() {
+  // Create a MyClass Object
+  MyClass object;
+  // Call the output member function
+  object.Output();
+  // This ends our program
+  return 0;
+}
