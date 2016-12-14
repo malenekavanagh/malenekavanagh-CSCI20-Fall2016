2320a9eaca0800ec2c02ae03c459ef20b58b4a5b
Malene Kavanagh <mkavanagh001@student.butte.edu>
2016-12-14 17:56:07 +0000

    lab20 reverted

--- lab20/lab20.cpp
+++ lab20/lab20.cpp
@@ -2,40 +2,40 @@
 Malene Kavanagh
 purpose: to comment and understand what the code is doing
 */
-#include <iostream>
-#include <cstring>
+#include <iostream> //opening iostream library
+#include <cstring> //opening c-string library
 using namespace std;
  
 int main()
 {
-                char firststr[10];
-                int firstname = 0;
-                char *head = firststr;
-                char *tail = firststr;
-                cout << "Please enter a 10 letter word or less" << endl;
-                cin >> firststr;
-                strlen(firststr);
-                firstname = strlen(firststr);
-                cout << "Your word is " << firststr << endl;
-                if (firstname<10)
+                char firststr[10]; //making an array
+                int firstname = 0; 
+                char *head = firststr; //making a pointer
+                char *tail = firststr; //making another pointer
+                cout << "Please enter a 10 letter word or less" << endl; //making sure it doesn't go over the letter count
+                cin >> firststr; //enters the user's word
+                strlen(firststr);  // returns length of cstring
+                firstname = strlen(firststr); //placing the cstring into an integer
+                cout << "Your word is " << firststr << endl; //outputs the name
+                if (firstname<10) //checking if the word is too long
                 {
                                 while (*head != '\0')
                                 {
-                                                cout << *head;
+                                                cout << *head; //outputs the word
                                                 head++;
                                 }
                 }
-                else
+                else //checking if the word is too long
                 {
                                 cout << "WARNING WORD TOO BIG TO DISPLAY!!" << endl;
                 }
                 cout << endl;
-                tail = &firststr[strlen(firststr) - 1];
+                tail = &firststr[strlen(firststr) - 1]; //inputs te entire word and flips word
                 if (firstname < 10)
                 {
                                 while (*tail>0)
                                 {
-                                                cout << *tail;
+                                                cout << *tail; //outputs the  flipped word
                                                 tail--;
                                 }
                 }
@@ -44,7 +44,7 @@ int main()
                 tail = &firststr[strlen(firststr) - 1];
                 head++;
                 tail--;
-                if (*head == *tail)
+                if (*head == *tail) // checking if the words are similar deciding if they are a palindrome or not
                 {
                                 cout << "It is an palindrome!" << endl;
                 }
@@ -55,3 +55,4 @@ int main()
  
                 return 0;
 }
+//code ends
--- program5/program5.cpp
+++ program5/program5.cpp
@@ -44,7 +44,7 @@ int main() {
   std::cout << "play either tic-tac-toe or a number guessing game" << std::endl;
   std::cout << "type either tictactoe or numbers" << std::endl;
   std::cin >> gameNames; //getline(cin, gameNames); get line code not needed
-  char tic_tac_toe[3][3];
+  
   if(gameNames == "numbers" || gameNames == "Numbers"){ //rnad numbers + instuctions on how to play
   int Num;
   int userNum;
@@ -71,25 +71,50 @@ int main() {
     }
   }
   if(gameNames == "tictactoe" || gameNames == "Tictactoe" || gameNames == "TicIacToe"){
+    char tic_tac_toe[3][3] = {' '};
     std::cout << "you play three rounds  then you get your points for how many times you won" << std::endl;
+    std::cout << "the spaces are numbered 1-3 for coloum first and 1-3 for the rows second" << std::endl;
     int turn = 0;
     int timesWon = 0;
-      int coloum;
-      int row;
-      int I = 0;
-      int gamesPlayed = 0;
-      while(I < 9 && gamesPlayed < 3){
-        int num1;
-        int num2;
-        while(turn == 0){
+    int coloum;
+    int row;
+    int I = 0;
+      
+    while(I < 9 && timesWon == 0){
+      int num1;
+      int num2;
+      //this checks if they won
+      // if((tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][0] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[0][1] == 'O' && tic_tac_toe[0][2] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O')  ){
+      //   std::cout << " you won " << std::endl;
+      //   timesWon++;
+      //   // not working gamesPlayed++;
+      //   for(int c = 0; c < 3; c++){
+      //     for(int r = 0; r < 3; r++){
+      //       tic_tac_toe[c][r] = ' ';
+      //     }
+      //   }
+      // }
+      // //this checks if they lost
+      // if((tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][0] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[0][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][1] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X')  ){
+      //   std::cout << " you lost this round" << std::endl;
+      //   timesWon--;
+      //   // not working gamesPlayed++;
+      //   for(int c = 0; c < 3; c++){
+      //     for(int r = 0; r < 3; r++){
+      //       tic_tac_toe[c][r] = ' ';
+      //     }
+      //   }
+      // }
+      //not working int gamesPlayed = 0;
+      while(turn == 0){
           
-          std::cout << "the spaces are numbered 1-3 for coloum first and 1-3 for the rows second" << std::endl;
+          
           std::cin >> coloum;
           std::cin >> row;
           coloum --;
           row--;
           if(tic_tac_toe[coloum][row] != 'O' && tic_tac_toe[coloum][row] != 'X'){
-          tic_tac_toe[coloum][row] = 'O';
+            tic_tac_toe[coloum][row] = 'O';
           std::cout << " " << endl;
           std::cout << "       " << tic_tac_toe[0][0] << "  |  " << tic_tac_toe[1][0] << "  |  " << tic_tac_toe[2][0] << std::endl;
 		      std::cout << "     -----+-----+-----" << std::endl;
@@ -103,49 +128,81 @@ int main() {
           }
         }
         
-        //this checks if they won
-        if((tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][0] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[0][1] == 'O' && tic_tac_toe[0][2] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O')  ){
-          std::cout << " you won this round" << std::endl;
-          timesWon++;
-          gamesPlayed++;
-          for(int c = 0; c < 3; c++){
-            for(int r = 0; r < 3; r++){
-              tic_tac_toe[c][r] = ' ';
-            }
+      //this checks if they won
+      if((tic_tac_toe[0][0] == 'O' && tic_tac_toe[0][1] == 'O' && tic_tac_toe[0][2] == 'O') || (tic_tac_toe[1][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[1][2] == 'O') || (tic_tac_toe[2][0] == 'O' && tic_tac_toe[2][1] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[2][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[0][2] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][0] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O')  ){
+        std::cout << " you won" << std::endl;
+        timesWon++;
+        // not working gamesPlayed++;
+        for(int c = 0; c < 3; c++){
+          for(int r = 0; r < 3; r++){
+            tic_tac_toe[c][r] = ' ';
           }
         }
-        //this checks if they lost
-        if((tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][0] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[0][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][1] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X')  ){
-          std::cout << " you lost this round" << std::endl;
-          gamesPlayed++;
-          for(int c = 0; c < 3; c++){
-            for(int r = 0; r < 3; r++){
-              tic_tac_toe[c][r] = ' ';
-            }
+      }
+      //this checks if they lost
+      else if((tic_tac_toe[0][0] == 'X' && tic_tac_toe[0][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[1][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[1][2] == 'X') || (tic_tac_toe[2][0] == 'X' && tic_tac_toe[2][1] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[2][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][0] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][1] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X')  ){
+        std::cout << " you lost" << std::endl;
+        timesWon--;
+        // not working gamesPlayed++;
+        for(int c = 0; c < 3; c++){
+          for(int r = 0; r < 3; r++){
+            tic_tac_toe[c][r] = ' ';
           }
         }
-        while(turn == 1){
+      }
+      while(turn == 1 && timesWon == 0){
+        num1 = ((rand()% 2) + 0);
+        num2 = ((rand()% 2) + 0);
+        while(tic_tac_toe[num1][num2] == 'O' || tic_tac_toe[num1][num2] == 'X'){
           num1 = ((rand()% 2) + 0);
           num2 = ((rand()% 2) + 0);
-          while(tic_tac_toe[num1][num2] == 'O' || tic_tac_toe[num1][num2] == 'X'){
-            num1 = ((rand()% 2) + 0);
-            num2 = ((rand()% 2) + 0);
-          }
-          if(tic_tac_toe[num1][num2] != 'O' && tic_tac_toe[num1][num2] != 'X'){
-            tic_tac_toe[num1][num2] = 'X';
-          }
-          
-          std::cout << " " << std::endl;
-          std::cout << "       " << tic_tac_toe[0][0] << "  |  " << tic_tac_toe[1][0] << "  |  " << tic_tac_toe[2][0] << std::endl;
-		      std::cout << "     -----+-----+-----" << std::endl;
-		      std::cout << "       " << tic_tac_toe[0][1] << "  |  " << tic_tac_toe[1][1] << "  |  " << tic_tac_toe[2][1] << std::endl;
-		      std::cout << "     -----+-----+-----" << std::endl;
-		      std::cout << "       " << tic_tac_toe[0][2] << "  |  " << tic_tac_toe[1][2] << "  |  " << tic_tac_toe[2][2] << std::endl;
-		      turn = 0;
         }
-        I++;
+        if(tic_tac_toe[num1][num2] != 'O' && tic_tac_toe[num1][num2] != 'X'){
+            tic_tac_toe[num1][num2] = 'X';
+        }
+          
+      //   std::cout << " " << std::endl;
+      //   std::cout << "       " << tic_tac_toe[0][0] << "  |  " << tic_tac_toe[1][0] << "  |  " << tic_tac_toe[2][0] << std::endl;
+		    // std::cout << "     -----+-----+-----" << std::endl;
+		    // std::cout << "       " << tic_tac_toe[0][1] << "  |  " << tic_tac_toe[1][1] << "  |  " << tic_tac_toe[2][1] << std::endl;
+		    // std::cout << "     -----+-----+-----" << std::endl;
+		    // std::cout << "       " << tic_tac_toe[0][2] << "  |  " << tic_tac_toe[1][2] << "  |  " << tic_tac_toe[2][2] << std::endl;
+	      turn = 0;
       }
+      std::cout << " " << std::endl;
+      std::cout << "       " << tic_tac_toe[0][0] << "  |  " << tic_tac_toe[1][0] << "  |  " << tic_tac_toe[2][0] << std::endl;
+		  std::cout << "     -----+-----+-----" << std::endl;
+		  std::cout << "       " << tic_tac_toe[0][1] << "  |  " << tic_tac_toe[1][1] << "  |  " << tic_tac_toe[2][1] << std::endl;
+		  std::cout << "     -----+-----+-----" << std::endl;
+	    std::cout << "       " << tic_tac_toe[0][2] << "  |  " << tic_tac_toe[1][2] << "  |  " << tic_tac_toe[2][2] << std::endl;
+	    
+      //this checks if they won
+      if((tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][0] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][2] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][0] == 'O') || (tic_tac_toe[0][0] == 'O' && tic_tac_toe[0][1] == 'O' && tic_tac_toe[0][2] == 'O') || (tic_tac_toe[0][1] == 'O' && tic_tac_toe[1][1] == 'O' && tic_tac_toe[2][1] == 'O') || (tic_tac_toe[0][2] == 'O' && tic_tac_toe[1][2] == 'O' && tic_tac_toe[2][2] == 'O')  ){
+        std::cout << " you won" << std::endl;
+        timesWon++;
+        // not working gamesPlayed++;
+        for(int c = 0; c < 3; c++){
+          for(int r = 0; r < 3; r++){
+            tic_tac_toe[c][r] = ' ';
+          }
+        }
+      }
+      //this checks if they lost
+      else if((tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][0] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][2] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][0] == 'X') || (tic_tac_toe[0][0] == 'X' && tic_tac_toe[0][1] == 'X' && tic_tac_toe[0][2] == 'X') || (tic_tac_toe[0][1] == 'X' && tic_tac_toe[1][1] == 'X' && tic_tac_toe[2][1] == 'X') || (tic_tac_toe[0][2] == 'X' && tic_tac_toe[1][2] == 'X' && tic_tac_toe[2][2] == 'X')  ){
+        std::cout << " you lost" << std::endl;
+        timesWon--;
+        // not working gamesPlayed++;
+        for(int c = 0; c < 3; c++){
+          for(int r = 0; r < 3; r++){
+            tic_tac_toe[c][r] = ' ';
+          }
+        }
+      }
+      I++;
+    }
     points.SetPoints(timesWon); 
-    std::cout << "you got " << points.GetTacPoints() << " points"  << std::endl;
+    std::cout << "you got " << points.GetTacPoints() << " points."  << std::endl;
   }
+  
+  
 }
